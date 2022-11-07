d2c8ba2503ae4390d7fa8022d0f6f40aea4aa6ee
2sssg <lsg020300@naver.com>
2022-11-07 13:28:17 +0900

    first init

---
+++ src/test/java/uni/employee_search_system/employee_search_system/EmployeeSearchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.employee_search_system.employee_search_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSearchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
